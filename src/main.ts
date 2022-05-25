5f9d6b52a1011f85f07ae9ac1c805a928ed1284c
minamorl <minamorl@users.noreply.github.com>
2022-05-25 15:33:04 +0900

    Make as component lib

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { App, DefineComponent } from 'vue'
+import components from './components'
 
-createApp(App).mount('#app')
+const plugin = {
+  install (Vue: App) {
+    for (const prop in components) {
+      if (components.hasOwnProperty(prop)) {
+        const component = components[prop as keyof typeof components]
+        Vue.component(component.name, component)
+      }
+    }
+  }
+}
+
+export default plugin
